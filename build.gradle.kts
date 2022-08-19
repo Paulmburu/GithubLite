7d0f42341daf3695ee130a150d1c12d7d795a3a1
Paul Mburu <paul.mburu@volane.com>
2022-08-20 00:59:51 +0300

    feat: setup Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+
+        classpath(Libraries.GradlePlugin.androidGradlePlugin)
+        classpath(Libraries.Kotlin.gradlePlugin)
+        classpath(Libraries.Google.Hilt.gradlePlugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
+}
